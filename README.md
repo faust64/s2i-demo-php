9732cd7ba806ba4437b1a748627796524d799605
Samuel MARTIN MORO <faust64@gmail.com>
2019-10-02 08:19:59 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# OpenShift S2I Demo
+
+```
+oc new-app php~https://github.com/faust64/s2i-demo-php
+```
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<html>";
+echo "<head><title>Demo S2I</title></head>";
+echo "<body><p><h1>Demo</h1></p><p><h2>S2I</h2></p>";
+echo "</body>";
+echo "</html>";
+?>
